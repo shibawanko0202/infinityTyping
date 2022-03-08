fbd06d511be27588a35ed0a456cd1a9b0e5e966b
shibawanko0202 <97938730+shibawanko0202@users.noreply.github.com>
2022-03-08 18:24:05 +0900

    単語の意味を表示する機能の実装

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@
   const untype = document.getElementById("untype");
   const score = document.getElementById("score");
   const bad = document.getElementById("bad");
-  const miss = document.getElementById("miss");
+  const more = document.getElementById("more");
   const accuracy = document.getElementById("accuracy"); 
   const ar = document.getElementById("ar");
   const main = document.getElementById("main");
@@ -39,76 +39,17 @@
   //ミスタイプのキーリスト
   const missType = [];
 
-  //問題集
-  const questions = [
-    "helloworld",
-    "googlechrome",
-    "google",
-    "facebook",
-    "amazon",
-    "apple",
-    "microsoft",
-    "javascript",
-    "cascadingstylesheets",
-    "hypertextmarkuplanguage",
-    "usestrict",
-    "document",
-    "listener",
-    "getelementbyid",
-    "textcontent",
-    "addeventlistener",
-    "current",
-    "function",
-    "font-family",
-    "font-size",
-    "justify-content",
-    "padding",
-    "margin",
-    "display",
-    "stylesheet",
-    "text-align",
-    "index.html",
-    "main.js",
-    "style.css",
-    "referenceerror",
-    "console",
-    "window",
-    "github",
-    "viewport",
-    "charset",
-    "header",
-    "body",
-    "footer",
-    "aside",
-    "typography",
-    "return",
-    "true",
-    "false",
-    "documentobjectmodel",
-    "classlist",
-    "foreach",
-    "new",
-    "constructor",
-    "this",
-    "transition",
-    "transform",
-    "div",
-    "title",
-    "math",
-    "random",
-    "length",
-
-  ];
-
   //正誤カウント
-  let scoreCount =0;
+  let scoreCount = 0;
   let badCount = 0;
   let accuracyRate;
 
   //問題のセット
   function q(){
-    untype.textContent = questions[Math.floor(Math.random() * questions.length)];
+    const q = questions.splice(Math.floor(Math.random() * questions.length),1)[0]
+    untype.textContent = q.word;
     typed.textContent = "";
+    more.textContent = q.mean;
   };
 
   //パーセンテージの表示
@@ -130,9 +71,19 @@
     let balloon = document.createElement("div");
     balloon.className = "balloon";
     balloon.id = `${key}`;
-    balloon.style.top = "calc(" + 1 * Math.random() * 100 + "%)";
-    balloon.style.left = "calc(" + 1 *Math.random() * 100 + "%)";
+    balloon.style.top = `${Math.random() * 100}%`;
+    balloon.style.left = `${Math.random() * 100}%`;
     balloon.textContent = `${key}`;
+    //カーソルをのせたら数値を表示
+    balloon.addEventListener("mouseenter",()=>{
+      balloon.style.fontSize = "10px";
+      let mt = missType.find((v) => v.key === balloon.textContent).num;
+      balloon.textContent += `:${mt}`
+    });
+    balloon.addEventListener("mouseleave",()=>{
+      balloon.style.fontSize = "14px";
+      balloon.textContent = `${key}`;
+    });
     main.appendChild(balloon);
   };
 
@@ -195,13 +146,12 @@
       badSound.play();
 
       //ミスタイプのキーをカウント
-      if(miss.textContent.match(e.key)){ //すでにあるなら加点
+      if(missType.find((v) => v.key === e.key)){ //すでにあるなら加点
         let mt = missType.find((v) => v.key === e.key).num;
 
-        miss.textContent = miss.textContent.replace(`${e.key}:${mt}`,`${e.key}:${mt + 1}`);
         missType.find((v) => v.key === e.key).num++;
 
-        document.getElementById(`${e.key}`).style.transform = `scale(${(missType.find((v) => v.key === e.key).num)})`;
+        document.getElementById(`${e.key}`).style.transform = `scale(${(missType.find((v) => v.key === e.key).num) * 0.9})`;
 
       } else { //初めてのミスキーカウント
         missBaloon(e.key);
@@ -209,7 +159,6 @@
           key:e.key,
           num:1,
         });
-        miss.textContent += `${missType[missType.length - 1].key}:${missType[missType.length - 1].num}  `;
       };
     };
   });
