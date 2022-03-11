551ab517f14032f909bdfa7583c8bea8fe9ad3dc
shibawanko0202 <97938730+shibawanko0202@users.noreply.github.com>
2022-03-11 18:02:22 +0900

    バルーンをクリックすると破裂するアニメーションの実装

--- js/main.js
+++ js/main.js
@@ -25,7 +25,7 @@
   const mean = document.getElementById("mean");
   const accuracy = document.getElementById("accuracy"); 
   const rate = document.getElementById("rate");
-  const main = document.getElementById("main");
+  const balloonRoom = document.getElementById("balloonroom");
   const hambarger = document.getElementById("hambarger");
   const overlay = document.getElementById("overlay");
   const close = document.getElementById("close");
@@ -84,7 +84,17 @@
       balloon.style.fontSize = "14px";
       balloon.textContent = `${key}`;
     });
-    main.appendChild(balloon);
+    //クリックしたら破裂
+    balloon.addEventListener("click",()=>{
+      balloon.classList.add("explosion");
+      badSound.currentTime = 0;
+      badSound.play();
+      //アニメーションが終了したら要素を消す
+      balloon.addEventListener("animationend",()=>{
+        balloon.classList.add("disabled");
+      });
+    });
+    balloonRoom.appendChild(balloon);
   };
 
   //ハンバーガーメニュー
@@ -148,9 +158,11 @@
       //ミスタイプのキーをカウント
       if(missType.find((v) => v.key === e.key)){ //すでにあるなら加点
         missType.find((v) => v.key === e.key).num++;
+        const missKey = document.getElementById(`${e.key}`);
         //大きくする倍率指定
-        document.getElementById(`${e.key}`).style.transform = `scale(${(missType.find((v) => v.key === e.key).num) * 0.9})`;
-
+        missKey.style.transform = `scale(${(missType.find((v) => v.key === e.key).num) * 0.9})`;
+        //破裂させたバルーンを復活
+        missKey.classList.remove("explosion","disabled");
       } else { //初めてのミスキーカウント
         createBalloon(e.key);
         missType.push({
